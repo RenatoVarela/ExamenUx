d6a17a1fbcd3c42fff45703cea30806e1218f15f
RenatoVarela <renatovarela13@gmail.com>
2020-08-23 02:57:24 -0600

    Updates

---
+++ precache-manifest.41f962152c3d88128801802471388aae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00150a4aba41086692e4f58e89355ca5",
+    "url": "/ExamenUx/index.html"
+  },
+  {
+    "revision": "04d3770a4fca2c584e2e",
+    "url": "/ExamenUx/static/css/main.4bb5f792.chunk.css"
+  },
+  {
+    "revision": "31410190ac4cff189488",
+    "url": "/ExamenUx/static/js/2.b0346982.chunk.js"
+  },
+  {
+    "revision": "301056a7ca16ead6156b38e6917dbb28",
+    "url": "/ExamenUx/static/js/2.b0346982.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04d3770a4fca2c584e2e",
+    "url": "/ExamenUx/static/js/main.25019860.chunk.js"
+  },
+  {
+    "revision": "47df570dd5f0d8dc7fb2",
+    "url": "/ExamenUx/static/js/runtime-main.1f7a6a1e.js"
+  },
+  {
+    "revision": "c55e5bcdef829b2766380384f9f19c14",
+    "url": "/ExamenUx/static/media/wallpaperflare.com_wallpaper.c55e5bcd.jpg"
+  }
+]);
